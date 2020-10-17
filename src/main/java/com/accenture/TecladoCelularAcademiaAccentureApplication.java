7cde2ecfbbd4899081c0e5a63df0f4af11ed0ad9
Anderson Oliveira <anderson.aodes@icloud.com>
2020-10-17 15:31:01 -0300

    primeiro commit

---
+++ src/main/java/com/accenture/TecladoCelularAcademiaAccentureApplication.java
@@ -0,0 +1,13 @@
+package com.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecladoCelularAcademiaAccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecladoCelularAcademiaAccentureApplication.class, args);
+	}
+
+}
